c38b3f69f7398b9f133b2e7b38ae1d92fc1426b5
Cuantu98 <stefano.merendino98@gmail.com>
2026-04-18 01:08:09 -0300

    feat: online feature set, major bugs to be fixed

--- src/Games/BattleShip.ts
+++ src/Games/BattleShip.ts
@@ -5,11 +5,17 @@ export function battleShipEvents(socket: Socket, io: Server){
         socket.join(lobby);
     });
 
-    socket.on("PlayerReadyBS", (lobby: string) => {
-        console.log(`Player in lobby ${lobby} is ready!`);
+    socket.on("PlayerReadyBS", (lobby: string, count:number) => {
+        socket.broadcast.to(lobby).emit("isReadyBS", count);
+    });
+
+    socket.on("BothReadyBS", async(lobby: string) => {
         const room = io.sockets.adapter.rooms.get(lobby);
         if (room && room.size === 2) {
-            socket.broadcast.to(lobby).emit("StartGameBS");
+            io.to(lobby).emit("StartGameBS");
+            const ids = await io.in(lobby).allSockets();
+            const idsArray = [...ids]; 
+            io.to(idsArray[Math.floor(Math.random() * (2))]).emit("GrantTurnBS");
         }
     });
 
@@ -17,20 +23,19 @@ export function battleShipEvents(socket: Socket, io: Server){
         socket.broadcast.to(lobby).emit("PackBoard");
     });
 
-    socket.on("SendBoard", (data: { lobby: string, board: any, ships: any }) => {
-        socket.broadcast.to(data.lobby).emit("ReceiveEnemyBoard", {
+    socket.on("SendBoard", (data: { roomId: string, board: any, ships: any }) => {
+        socket.broadcast.to(data.roomId).emit("ReceiveEnemyBoard", {
             board: data.board,
             ships: data.ships,
         });
     });
+    
+    socket.on("SwitchTurnBS", (lobby: string) => {
+        socket.broadcast.to(lobby).emit("GrantTurnBS");
+    });
 
-    socket.on("RequestTurnBS", (lobby: string) => {
-        const room = io.sockets.adapter.rooms.get(lobby);
-        if (room && room.size === 2) {
-            const ids = Array.from(room);
-            const randomIndex = Math.floor(Math.random() * 10) % 2;
-            io.to(ids[randomIndex]).emit("GrantTurn");
-        }
+    socket.on("SendAttack", (lobby:string, row:number, col:number)=>{
+        socket.broadcast.to(lobby).emit("ReceiveAttack", row, col);
     });
 
 }
